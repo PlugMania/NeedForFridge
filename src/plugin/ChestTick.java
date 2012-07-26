22cad20572545f18606485c4fa25d0aae1feaf4a
Mathias Schmidt <schmidt.mathias.87@gmail.com>
2012-07-26 16:05:10 +0200

    Removed debugging messages.

--- src/plugin/ChestTick.java
+++ src/plugin/ChestTick.java
@@ -20,35 +20,26 @@ public class ChestTick{
 		
 		server.getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable(){
 			public void run(){
-				server.broadcastMessage("Tick!");
 				for(Chest c:chests.keySet()){
-					server.broadcastMessage("Enter Chest ->");
 					if(Utility.containsFood(c.getInventory())){
-						server.broadcastMessage("Contains Food!");
 						int value = chests.get(c);
-						server.broadcastMessage("Start Value: "+value);
 						value = value - Utility.getRotValue(c.getLocation().getBlock().getBiome());
-						server.broadcastMessage("Value after subtract: "+value);
 						while(((value + 100) <= 3600) && (c.getInventory().contains(Material.SNOW_BALL))){
 							value = value + 100;
 							ItemStack is = c.getInventory().getItem(c.getInventory().first(Material.SNOW_BALL));
 							is.setAmount(is.getAmount()-1);
-							server.broadcastMessage("Snow Ball!");
 						}
 						while(((value + 1000) <= 3600) && (c.getInventory().contains(Material.SNOW_BLOCK))){
 							value = value + 1000;
 							ItemStack is = c.getInventory().getItem(c.getInventory().first(Material.SNOW_BLOCK));
 							is.setAmount(is.getAmount()-1);
-							server.broadcastMessage("Snow Block!");
 						}
 						while(((value + 2400) <= 3600) && (c.getInventory().contains(Material.ICE))){
 							value = value + 2400;
 							ItemStack is = c.getInventory().getItem(c.getInventory().first(Material.ICE));
 							is.setAmount(is.getAmount()-1);
-							server.broadcastMessage("Ice!");
 						}
 						chests.put(c, value);
-						server.broadcastMessage("New Value: "+value);
 						if(value <= 0){
 							Utility.rotFood(c.getInventory(), c.getLocation().getBlock().getBiome());
 						}
